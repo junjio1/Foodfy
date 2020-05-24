330994446d5aa089fa00712977f7c1d3bf4e95b0
Junji Oshiro <junjio1@hotmail.com>
2020-05-24 17:00:03 -0300

    data.jason created js.writefile add to admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -19,8 +19,15 @@ exports.edit = function (req, res) {
 
 }
 exports.post = function (req, res) {
-    return res.send(req.body)
+    const keys = Object.keys(req.body)
 
+    for (key of keys){
+        if (req.body[key] == ""){
+             return res.send("Preencha todos os campos")
+        }
+    }
+
+    return res.send(req.body)
 }
 exports.put = function (req, res) {
     return res.send("ok")
